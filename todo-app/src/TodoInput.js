ecae5d414ebcfb8bda6af1e8d243b02c472932e8
佐藤利徳 <tyahhahha@gmail.com>
2018-04-13 23:27:10 +0900

    todo app: avalable adding dynamic new todo

--- todo-app/src/TodoInput.js
+++ todo-app/src/TodoInput.js
@@ -3,17 +3,31 @@ import React, {Component} from 'react'
 export default class TodoInput extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      inputValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
 
+  handleChange(e) {
+    this.setState({inputValue: e.target.value})
+  }
+
   handleClick() {
-    this.props.addTodo('新規TODO')
+    this.props.addTodo(this.state.inputValue)
+    this.setState({inputValue: ''})
   }
 
   render() {
     return (
       <div>
-        <input placeholder="新規のTODOを入力してください" />
+        <input
+          placeholder="新規のTODOを入力してください"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
         <button onClick={this.handleClick}>登録</button>
       </div>
     )
