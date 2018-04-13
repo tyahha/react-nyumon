b9058d2b491eb641b61849bcf60ea3a2117400c7
佐藤利徳 <tyahhahha@gmail.com>
2018-04-13 23:19:07 +0900

    todo app: avalable adding static todo

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,16 +3,36 @@ import TodoInput from './TodoInput'
 import TodoList from './TodoList'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: [
+        {title: '1つめのTODO', id: 0},
+        {title: '2つめのTODO', id: 1},
+      ],
+      uniqueId: 1,
+    }
+
+    this.addTodo = this.addTodo.bind(this)
+  }
+
+  addTodo(title) {
+    const {
+      tasks,
+      uniqueId,
+    } = this.state
+
+    tasks.push({title, id: uniqueId})
+
+    this.setState({tasks, uniqueId: uniqueId + 1})
+  }
+
   render() {
-    const tasks = [
-      {title: '1つめのTODO', id: 0},
-      {title: '2つめのTODO', id: 1},
-    ]
     return (
       <div>
         <h1>TODO App</h1>
-        <TodoInput />
-        <TodoList tasks={tasks} />
+        <TodoInput addTodo={this.addTodo}/>
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
--- todo-app/src/TodoInput.js
+++ todo-app/src/TodoInput.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react'
 
 export default class TodoInput extends Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.props.addTodo('新規TODO')
+  }
+
   render() {
     return (
       <div>
         <input placeholder="新規のTODOを入力してください" />
-        <button>登録</button>
+        <button onClick={this.handleClick}>登録</button>
       </div>
     )
   }
