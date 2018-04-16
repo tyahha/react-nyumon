e8c2abfe4a9d1358d4d23c34fab277aa6af16fe8
佐藤利徳 <tyahhahha@gmail.com>
2018-04-17 00:00:56 +0900

    add redux logger middleware

--- redux-first/src/index.js
+++ redux-first/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { logger } from 'redux-logger'
 import tasksReducer from './reducers/tasks'
 import TodoApp from './containers/TodoApp'
 import { render } from 'react-dom'
 
-const store = createStore(tasksReducer)
+const store = createStore(
+  tasksReducer,
+  applyMiddleware(logger)
+)
 
 render(
   <Provider store={store}>
